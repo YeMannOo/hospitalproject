91b07959a0e431e015c7e1b345d23cf06ce032d4
Ye Mann Oo <yemannoo777@gmail.com>
2022-08-04 21:54:10 +0630

    4/Aug/2022 10:00 pm

---
+++ js/app.js
@@ -0,0 +1,33 @@
+$(document).ready	(function(){
+	// console.log('hay i am working');
+
+	// Start Header Section
+
+	// Start Banner Section
+	// $(".carousel").carousel({
+	// 	interval:1000;
+	// });
+
+	// End Banner Section
+
+	// End Header Section
+
+	// Start Info Section
+	$(window).scroll(function(){
+		let getscrollpoint = $(this).scrollTop();
+		console.log(getscrollpoint);
+
+		if(getscrollpoint >=329){
+			$(".infotexts").addClass("fromlefts");
+			$(".infopics").addClass("fromrights");
+
+		}else{
+			$(".infotexts").removeClass("fromlefts");
+			$(".infopics").removeClass("fromrights");
+		}
+
+	});
+	// End Info Section 
+
+
+});
